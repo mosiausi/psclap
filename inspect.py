bf7652f6a3aa2b883f4145fb49ed5d87811def85
dorz5 <41587920+dorz5@users.noreply.github.com>
2020-07-27 21:52:09 +0100

    Update and rename mnping.py to inspect.py

--- inspect.py
+++ inspect.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 import subprocess
 import platform
+import os
 
 current_ip_address = ['192.168.0.37']
+
 
 def ping_ip(current_ip_address):
         try:
             output = subprocess.check_output("ping -{} 1 {} -i 0.2 -s 1 -W 1".format('n' if platform.system().lower(
-            ) == "windows" else 'c', current_ip_address ), shell=True, universal_newlines=True)
+            ) == "windows" else 'c', current_ip_address), shell=True, universal_newlines=True)
             if 'unreachable' in output:
                 return False
             else:
@@ -18,9 +20,9 @@ def ping_ip(current_ip_address):
 if __name__ == '__main__':
     for each in current_ip_address:
         if ping_ip(each):
-            print(f"{each} is available")
+            print(f"{each} is available.\n")
             import netstat
-#            exec("mnnetstat.py")
-#            exec(open("/home/pi/scripts/mnnetstat.py").read())
+#            exec("netstat.py")
+#            exec(open("/home/pi/scripts/netstat.py").read())
         else:
-            print(f"{each} is not available")
+            print(f"{each} is not available.")
