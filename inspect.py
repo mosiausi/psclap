049d145c7eb7eb761410ae25f9208e578c759b45
dorz5 <41587920+dorz5@users.noreply.github.com>
2020-07-28 19:22:01 +0100

    Update inspect.py

--- inspect.py
+++ inspect.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python3
 import subprocess
 import platform
-import os
 
 current_ip_address = ['192.168.0.37']
 
 
 def ping_ip(current_ip_address):
-        try:
-            output = subprocess.check_output("ping -{} 1 {} -i 0.2 -s 1 -W 1".format('n' if platform.system().lower(
-            ) == "windows" else 'c', current_ip_address), shell=True, universal_newlines=True)
-            if 'unreachable' in output:
-                return False
-            else:
-                return True
-        except Exception:
-                return False
+    try:
+        output = subprocess.check_output("ping -{} 1 {} -i 0.2 -s 1 -W 1".format('n' if platform.system().lower(
+        ) == "windows" else 'c', current_ip_address), shell=True, universal_newlines=True)
+        if 'unreachable' in output:
+            return False
+        else:
+            return True
+    except (ValueError, Exception):
+        return False
+
 
 if __name__ == '__main__':
     for each in current_ip_address:
         if ping_ip(each):
-            print(f"{each} is available.\n")
+            print(each, "is available.\n")
             import deepinspection
-#            exec("deepinspection.py")
-#            exec(open("/home/pi/scripts/deepinspection.py").read())
         else:
-            print(f"{each} is not available.")
-        
+            print(each, "is not available.")
