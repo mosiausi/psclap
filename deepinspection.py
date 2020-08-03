4cd805656696ec343c59ab7fba818a88453317d7
mnayman <dzevel5@yahoo.com>
2020-08-03 16:49:04 +0100

    V1.0

--- deepinspection.py
+++ deepinspection.py
@@ -10,11 +10,7 @@ commands = [
     " sort | uniq -c | sort -n | grep ESTABLISHED | awk '{print $1}'"
 ]
 
-# monitor = [
-# "netstat -tun | grep -v '127.0.0.1\|192.168.0.15' | uniq -c | sort -n"
-# ]
-
-actions = ("uname")
+actions = 'poweroff'
 
 # initialize the SSH client
 client = paramiko.SSHClient()
@@ -22,24 +18,19 @@ client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 try:
     client.connect(hostname=hostname, username=username, password=password)
-except IndexError:
-    print("[!] Cannot connect to the SSH Server")
+except Exception as e:
+    print(e, "[!]")
     exit()
 # execute the commands
 for command in commands:
     # print("="*50, command, "="*50)
     stdin, stdout, stderr = client.exec_command(command)
     conn = int(stdout.read().decode())
-    if (conn < 5):
+    if conn < 5:
         print("Less than ({})".format(conn), "connections."
               " Deploy: <{}>".format(actions), "command using SSH."),
         stdin, stdout, stderr = client.exec_command(actions)
         print(stdout.read().decode())
-    elif (conn > 3):
+    elif conn > 3:
         print("More than ({})".format(conn), "connections."
               " This program will be terminated")
-    err = stderr.read().decode()
-    if err:
-        print(err)
-
-client.close()
